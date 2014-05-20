8005f680614384c5f9af10fedbe7ad5cef7aae8e
Jack Chen <massd.me@gmail.com>
2014-05-20 22:49:17 +0100

    finished sell side deal match, need to do buy, but still confusion

--- trade.ml
+++ trade.ml
@@ -34,7 +34,7 @@ module type Platform = sig
   val start : unit -> platform_t
   val new_order : platform_t -> acc_id:account_id_t -> stock:stock_t -> order_type:order_type_t -> price:price_t -> vol:vol_t -> order_id_t option * platform_t
   val amend_order : platform_t -> order_id:order_id_t -> price:price_t -> vol:vol_t -> order_id_t option * platform_t
-  val cancel_order : platform_t -> order_id:order_id_t -> orderid_t option * platform_t
+  val cancel_order : platform_t -> order_id:order_id_t -> order_id_t option * platform_t
     
   (*val all_stocks : platform_t -> stock_t list
   val query_stock : platform_t -> stock_t -> price_t
@@ -58,21 +58,18 @@ module Godxilla : Platform = struct
 		 }
 
   type account_t = { id : account_id_t;
-		     orders : order_t Int.Map.t;
 		     holdings : vol_t String.Map.t;
 		   }
 
-  type platform_t = { buy : order_t Int.Map.t;
-		      sell : order_t Int.Map.t;
+  type platform_t = { orders : order_t Int.Map.t;
 		      accounts : account_t Int.Map.t;
 		      stocks : price_t String.Map.t;
 		    }
       
-  let start () = { buy = Int.Map.empty; sell = Int.Map.empty; accounts = Int.Map.empty; stocks = String.Map.empty; }
+  let start () = { orders = Int.Map.empty; accounts = Int.Map.empty; stocks = String.Map.empty; }
     
   let create_account acc_id = 
     { id = acc_id;
-      orders = Int.Map.empty;
       holdings = String.Map.empty;
     }
     
@@ -88,83 +85,85 @@ module Godxilla : Platform = struct
     }
 
 (* Utilities *)
-  let find_order buy sell order_id = 
-    match Map.find buy order_id, Map.find sell order_id with
-      | Some _, Some _ -> raise (Internal_err "Same key existing in both buy and sell")
-      | Some order, None when order.status = Pending -> Some order, Some Buy
-      | None, Some order when order.status = Pending -> Some order, Some Sell
-      | _ -> None, None
+  let find_pending_order p order_id = 
+    match Map.find p.orders order_id with
+      | Some order when order.status = Pending -> Some order
+      | _ -> None
 
-  let find_account accounts acc_id = Map.find accounts acc_id
+  let find_account p acc_id = 
+    match Map.find p.accounts acc_id with
+      | Some acc -> p,acc
+      | None -> let acc = create_account acc_id in {p with accounts = Map.add p.accounts ~key:acc_id ~data:acc}, acc 
 
-  let update_order ({buy;sell;accounts;stocks} as p) ~order_id ~update ~update_accounts = 
-    match find_order buy sell order_id with
-      | None, _ | _, None -> None, p
-      | Some order, Some Buy -> None, {p with buy = update buy order; accounts = update_accounts accounts order}
-      | Some order, Some Sell -> None, {p with sell = update sell order; accounts = update_accounts accounts order}
+  (* Update order, used in amend and cancel *)
+  let update_order p ~order_id ~update = 
+     match find_pending_order p order_id with
+      | None -> p
+      | Some order -> 
+	let order' = { update order with timestamp = Unix.time(); } in
+	{p with orders = Map.add p.orders ~key:order_id ~data:order'}
 
-(* Deal match *)
-  let find_stock_orders orders stock =
-    let filter = fun (_, order) -> order.stock = stock && order.status = Pending in
-    let order_list = Map.to_alist orders |> List.filter ~f:filter in
-    order_list
-
-  let sort_buy bar ol = 
-    List.sort ~cmp:(
-      fun (_, o1) (_, o2) -> compare o1.timestamp o2.timestamp) ol
-  let sort_sell bar ol = 
-    List.sort ~cmp:(
-      fun (_, o1) (_, o2) -> compare (Float.abs(o1.price-.bar), o1.timestamp) (Float.abs(o2.price-.bar),o2.timestamp)) ol
-      
-
+  (* Stock position *) 
   let find_stock_pos p stock =
     match Map.find p.stocks stock with
       | None -> 0.
       | Some pos -> pos
 
-  let update_stock_pos p stock price =
-    Map.add p.stocks ~key:stock ~data:price
-      
-  
-      
+  let update_stock_pos p ~stock ~price =
+    {p with stocks = Map.add p.stocks ~key:stock ~data:price}
 
-(* New order *)
-  let new_order ({buy;sell;accounts} as p) ~acc_id ~stock ~order_type ~price ~vol = 
-    let order = create_order ~acc_id ~stock ~order_type ~price ~vol in
-    let acc = 
-      match find_account accounts acc_id with
-	| None -> let new_acc = create_account acc_id in {new_acc with orders = Map.add new_acc.orders ~key:order.id ~data:order}
-	| Some acc -> {acc with orders = Map.add acc.orders ~key:order.id ~data:order}
+  (* Deal match *)
+  let find_buys p ~filter = 
+    Map.to_alist p.orders |> List.filter ~f:filter |> List.map ~f:snd |> List.sort ~cmp:(fun o1 o2 -> -(compare o1.timestamp o2.timestamp))
+    
+  let update_account p ~acc_id ~stock ~vol =
+    let p, acc = find_account p acc_id in
+    let new_vol = 
+      match Map.find acc.holdings stock with
+      | None -> 0.
+      | Some v -> v +. vol
     in 
-    match order_type with
-      | Buy -> Some order.id, {p with buy = Map.add buy ~key:order.id ~data:order; accounts = Map.add accounts ~key:acc.id ~data:acc}
-      | Sell -> Some order.id, {p with sell = Map.add sell ~key:order.id ~data:order; accounts = Map.add accounts ~key:acc.id ~data:acc}
+    let acc = {acc with holdings = Map.add acc.holdings ~key:stock ~data:new_vol} in
+    {p with accounts = Map.add p.accounts ~key:acc_id ~data:acc}
 
-(* Amend order *)
-  let amend price vol side_map order =
-    let amended_order = {order with price = price; vol = vol; timestamp = Unix.time();} in
-    Map.add side_map ~key:order.id ~data:amended_order
+  let complete_order p ~order_id = update_order p ~order_id ~update:(fun order -> {order with status = Completed;})
 
-  let amend_in_accounts price vol accounts order =
-    match find_account accounts order.acc_id with
-      | None -> raise (Internal_err "Cannot find order in accounts")
-      | Some acc -> Map.add accounts ~key:order.acc_id ~data:{acc with orders = amend price vol acc.orders order}
+  let try_sell p ~sell =
+    if sell.order_type = Buy then assert false
+    else 
+      let buys = find_buys p ~filter:(fun (_, buy) -> buy.stock = sell.stock && buy.price >= sell.price) in
+      let rec loop p sell = function
+	| [] -> p
+	| buy::buys ->
+	  let pos = (buy.price +. sell.price) /. 2. in
+	  let leftover_vol = Float.abs (buy.vol -. sell.vol) in
+	  if buy.vol > sell.vol then
+	    complete_order p ~order_id:sell.id |> update_account ~acc_id:buy.id ~stock:buy.stock ~vol:(-.sell.vol) |> update_account ~acc_id:sell.id ~stock:sell.stock ~vol:sell.vol |> update_order ~order_id:buy.id ~update:(fun buy_order -> {buy_order with vol = leftover_vol}) |> update_stock_pos ~stock:sell.stock ~price:pos
+	  else if buy.vol = sell.vol then
+	    complete_order p ~order_id:sell.id |> complete_order ~order_id:buy.id |> update_account ~acc_id:buy.id ~stock:buy.stock ~vol:(-.sell.vol) |> update_account ~acc_id:sell.id ~stock:sell.stock ~vol:sell.vol |> update_stock_pos ~stock:sell.stock ~price:pos
+	  else 
+	    let p = 
+	      complete_order p ~order_id:buy.id |> update_account ~acc_id:buy.id ~stock:buy.stock ~vol:(-.buy.vol) |> update_account ~acc_id:sell.id ~stock:sell.stock ~vol:buy.vol |> update_order ~order_id:sell.id ~update:(fun sell_order -> {sell_order with vol = leftover_vol}) |> update_stock_pos ~stock:sell.stock ~price:pos
+	    in 
+	    loop p {sell with vol = leftover_vol} buys
+      in 
+      loop p sell buys
+ 
+      
+  (* New order *)
+  let new_order p ~acc_id ~stock ~order_type ~price ~vol = 
+    let order = create_order ~acc_id ~stock ~order_type ~price ~vol in
+    Some order.id, {p with orders = Map.add p.orders ~key:order.id ~data:order}
 
+
+
+  (* Amend order *)
   let amend_order p ~order_id ~price ~vol = 
-    update_order p ~order_id ~update:(amend price vol) ~update_accounts:(amend_in_accounts price vol)
+     Some order_id, update_order p ~order_id ~update:(fun order -> {order with price = price; vol = vol;})
 
-(* Cancel order *)
-  let cancel side_map order =
-    let cancelled_order = {order with status = Cancelled; timestamp = Unix.time();} in
-    Map.add side_map ~key:order.id ~data:cancelled_order
+  (* Cancel order *)
+  let cancel_order p ~order_id = None, update_order p ~order_id ~update:(fun order -> {order with status = Cancelled;})
 
-  let cancel_in_accounts accounts order =
-    match find_account accounts order.acc_id with
-      | None -> raise (Internal_err "Cannot find order in accounts")
-      | Some acc -> Map.add accounts ~key:order.acc_id ~data:{acc with orders = cancel acc.orders order}
-
-  let cancel_order p ~order_id = 
-    update_order p ~order_id ~update:cancel ~update_accounts:cancel_in_accounts
 end
 
 
